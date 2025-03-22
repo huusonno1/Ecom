2292253de10ab430af898a3bd97f3e27a6e69d93
huusonno1 <huuson1423@gmail.com>
2025-03-22 11:06:10 +0700

    feat: add shopping cart

--- fe-ecom/src/shared/components/Header/Header.tsx
+++ fe-ecom/src/shared/components/Header/Header.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { PopoverGroup } from "@headlessui/react";
+import ShoppingCart from "../Cart/ShoppingCart";
 
 export default function Example() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
   return (
     <header className="bg-[#FBEBB5]">
       <nav
@@ -66,7 +69,11 @@ export default function Example() {
               />
             </svg>
           </a>
-          <a className="shopping-cart" href="#">
+          <a
+            className="shopping-cart"
+            href="#"
+            onClick={() => setIsCartOpen(true)}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -81,6 +88,10 @@ export default function Example() {
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
               />
             </svg>
+            <ShoppingCart
+              isOpen={isCartOpen}
+              onClose={() => setIsCartOpen(false)}
+            />
           </a>
           <a href="#" className="text-lg font-semibold text-gray-900">
             Log in <span aria-hidden="true">&rarr;</span>
