927a2f962fa60a7a40176529e78e4919133f2b93
huusonno1 <huuson1423@gmail.com>
2025-03-29 16:06:19 +0700

    add page contact

--- fe-ecom/src/core/routes/routes.ts
+++ fe-ecom/src/core/routes/routes.ts
@@ -2,6 +2,7 @@ import HomePage from "../../pages/home/HomePage";
 import ShopPage from "../../pages/shop/ShopPage";
 import SingleProductPage from "../../pages/shop/SingleProductPage";
 import CheckoutPage from "../../pages/checkout/CheckoutPage";
+import ContactPage from "../../pages/contact/ContactPage";
 export interface IRoute {
   path: string;
   component: React.ComponentType;
@@ -12,6 +13,7 @@ const routes: IRoute[] = [
   { path: "/shop", component: ShopPage },
   { path: "/shop/idProduct", component: SingleProductPage },
   { path: "/checkout", component: CheckoutPage },
+  { path: "/contact", component: ContactPage },
 ];
 
 export default routes;
